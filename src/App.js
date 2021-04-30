9e1f516e0bed9bd04dfbdce065e61d70a991b0dd
mjchoi <eunoia2040@gmail.com>
2021-04-30 10:25:03 +0900

    implemente functions for  todo list

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useCallback, useState, useRef } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'react practice',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'component style',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'make a example',
+      checked: false,
+    }
+  ]);
+
+  // 고유값으로 사용될 id
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo => 
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        )
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+    </TodoTemplate>
+  );
+}
+
+export default App;
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline,
+} from 'react-icons/md';
+import className from 'classnames';
+import './TodoListItem.scss';
+
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+    const { id, text, checked } = todo;
+    return (
+        <div className="TodoListItem">
+            <div className={className('checkbox', {checked})} onClick={() => onToggle(id)}>
+                { checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                <div className="text">{text}</div>
+            </div>
+            <div className="remove" onClick={() => onRemove(id)}>
+                <MdRemoveCircleOutline />
+            </div>
+        </div>
+    )
+}
+
+export default TodoListItem;
